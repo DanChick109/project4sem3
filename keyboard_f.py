1ab9f322d414865304008d60ba6670af633c9461
DanChick109 <valkov.off05@gmail.com>
2024-12-06 16:48:23 +0300

    Made FIO-system, look4rates and some other changes

--- keyboard_f.py
+++ keyboard_f.py
@@ -1,20 +1,40 @@
-def keyboard_f(tb, stage: str, num: int = 1):
+def btfl_num(num):
+    dict_emo = {'1' : "1️⃣",
+                '2' : "2️⃣",
+                '3' : "3️⃣",
+                '4' : "4️⃣",
+                '5' : "5️⃣",
+                '6' : "6️⃣",
+                '7' : "7️⃣",
+                '8' : "8️⃣",
+                '9' : "9️⃣",
+                '0' : "0️⃣"}
+    s = ""
+    for i in str(num):
+        s += dict_emo[i]
+    return s
+
+def keyboard_f(tb, stage: str, num: int = 1, max_num: int = 2):
     if stage == "TEACHER_CHOOSING":
         keyboard = tb.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
         bttn1 = tb.types.KeyboardButton('Написать отзыв')
         bttn2 = tb.types.KeyboardButton('Посмотреть отзывы')
         bttn3 = tb.types.KeyboardButton('Посмотреть статистику')
-        bttn4 = tb.types.KeyboardButton('Назад')
+        bttn4 = tb.types.KeyboardButton('Убрать клавиатуру')
         keyboard.add(bttn1, bttn2, bttn3, bttn4)
     if stage == "TEACHER_BUTTON":
         keyboard = tb.types.ReplyKeyboardRemove()
     if stage == "TEACHER_LOOK":
         keyboard = tb.types.InlineKeyboardMarkup()
-        button_count = tb.types.InlineKeyboardButton(text=f"{1}")
-        button_save = tb.types.InlineKeyboardButton(text="Далее",
+        count_button = tb.types.InlineKeyboardButton(text=f"{btfl_num(num)}", callback_data="blank")
+        goon_button = tb.types.InlineKeyboardButton(text="Далее",
                                                     callback_data='go_on')
-        button_change = tb.types.InlineKeyboardButton(text="Назад",
+        gobck_button = tb.types.InlineKeyboardButton(text="Назад",
                                                     callback_data='go_back')
-        keyboard.add(button_count)
-        keyboard.add(button_save, button_change)
+        if num == 1:
+            keyboard.add(count_button, goon_button)
+        elif num == max_num:
+            keyboard.add(gobck_button, count_button)
+        else:
+            keyboard.add(gobck_button, count_button, goon_button)
     return keyboard
